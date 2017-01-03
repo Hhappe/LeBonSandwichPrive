290b143324b94d825c52bffd4298b2a0602e7738
Hugo <hugo-happe@hotmail.fr>
2017-01-03 16:51:07 +0100

    Modification de commande.php

--- commande.php
+++ commande.php
@@ -3,33 +3,33 @@
 require_once("conf/autoload.php");
 require_once("vendor/autoload.php");
 
-// Connexion à la BDD
+// Connexion ï¿½ la BDD
 $connexion = new AppInit();
 $connexion->bootEloquent("conf/config.ini");
 
 // Retour en JSON
 if(isset($_GET["id"]))
 {
-	$json = model\categorie::where('id', $_GET["id"])->get()->toJson();
+	$json = model\commande::where('id', $_GET["id"])->get()->toJson();
 	
 	header("Content-Type: application/json");
 	echo $json;
 }
 else
 {
-	if((isset($_POST["nom"])) && (isset($_POST["description"])))
+	if((isset($_POST["dateretrait"])) && (isset($_POST["etat"])))
 	{
-		$cat = new model\categorie();
-		$cat->nom = $_POST["nom"];
-		$cat->description = $_POST["description"];
+		$cat = new model\commande();
+		$cat->dateretrait = $_POST["dateretrait"];
+		$cat->description = $_POST["etat"];
 		$cat->save();
 		
 		http_response_code(201);
 	}
 	else
 	{
-		$res = model\categorie::select('id', 'nom')->get();
-		$tab = array("nb" => $res->count(), "categories" => $res);
+		$res = model\commande::select('id', 'dateretrait')->get();
+		$tab = array("nb" => $res->count(), "commandeses" => $res);
 		$json = json_encode($tab);
 		
 		header("Content-Type: application/json");
