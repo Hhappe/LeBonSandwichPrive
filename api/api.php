377a89b08980e9853df1ee25e95d866d367b5335
Charles Piguet <cpiguet@msn.com>
2017-01-24 18:08:28 +0100

    Documentation effectuée
    Toutes les stories effectuées.

--- api/api.php
+++ api/api.php
@@ -21,30 +21,48 @@ $app = new \Slim\App($c);
  *
  * @apiDescription Retourne un tableau contenant une représentation json de chaque commande.
  *
+ * @apiParam {Number} limit Nombre d'éléments à afficher
+ * @apiParam {Number} offset Pagination
+ * @apiParam {Number} etat Filtrage sur l'état
+ * @apiParam {String} date Filtrage sur la date de retrait (format yyyy-mm-dd)
+ *
  * @apiSuccess (Succès : 200) {Number} id Identifiant de la commande
  * @apiSuccess (Succès : 200) {Date} dateretrait Date de retrait de la commande
+ * @apiSuccess (Succès : 200) {Number} etat Etat de la commande
  * @apiSuccess (Succès : 200) {String} token Token de la commande
  * @apiSuccess (Succès : 200) {String} montant Montant de la commande
+ * @apiSuccess (Succès : 200) {Link} all Lien pour afficher toutes les commandes
+ * @apiSuccess (Succès : 200) {Link} previous Lien pour afficher les commandes précédentes
+ * @apiSuccess (Succès : 200) {Link} next Lien pour afficher les commandes suivantes
  *
  * @apiSuccessExample {json} exemple de réponse en cas de succès
  *     HTTP/1.1 200 OK
  *
- *	[
+ *	{
+ *		"commandes" :
+ *		[
+ *			{
+ *				"id": 1,
+ *				"dateretrait": "2017-01-04",
+ *				"etat": 1,
+ *				"token": 1546425,
+ *				"montant": 0
+ *			},
+ *			{
+ *				"id": 2,
+ *				"dateretrait": "2017-01-24",
+ *				"etat": 1,
+ *				"token": "174086",
+ *				"montant": 0
+ *			}
+ *		],
+ *		"links" :
  *		{
- *			"id": 1,
- *			"dateretrait": "2017-01-04",
- *			"etat": 1,
- *			"token": 1546425,
- *			"montant": 0
- *		},
- *		{
- *			"id": 2,
- *			"dateretrait": "2017-01-24",
- *			"etat": 1,
- *			"token": "174086",
- *			"montant": 0
+ *	 		"all" : { "href" : "/commandes" },
+ *			"previous" : { "href" : "/commandes?limit=20&offset=0" },
+ *			"next" : { "href" : "/commandes?limit=20&offset=20" }
  *		}
- *	]
+ *	}
  *
  */
 $app->get('/commandes',
@@ -106,6 +124,56 @@ $app->get('/commandes/{id}',
 	}
 )->setName('detailsCommande');
 
+/**
+ * @apiGroup Commandes
+ * @apiName changementEtat
+ * @apiVersion 0.1.0
+ *
+ * @api {post} /commandes/id  change l'état d'une commande
+ *
+ * @apiDescription Retourne une représentation json de l'état de la commande et le lien pour visualiser la commande.
+ *
+ * @apiSuccess (Succès : 200) {Number} etat Etat de la commande
+ * @apiSuccess (Succès : 200) {Link} details Lien pour afficher les détails de la commande
+ *
+ * @apiSuccessExample {json} exemple de réponse en cas de succès
+ *     HTTP/1.1 200 OK
+ *	{
+ *		"etat": { 2 },
+ *		"links": {
+ *			"details": {
+ *				"href": "/commandes/2"
+ *			}
+ *		}
+ *	}
+ *
+ * @apiError (Erreur : 404) error Commande inexistante
+ *
+ * @apiErrorExample {json} exemple de réponse en cas d'erreur
+ *     HTTP/1.1 404 Not Found
+ *
+ *     {
+ *       "error" : "commande introuvable : http://localhost/lbsprive/api/commandes/10"
+ *     }
+ *
+ * @apiError (Erreur : 400) error Etat incorrect
+ *
+ * @apiErrorExample {json} exemple de réponse en cas d'erreur
+ *     HTTP/1.1 400 Bad Request
+ *
+ *     {
+ *       "error" : "état incorrect: http://localhost/lbsprive/api/commandes/10"
+ *     }
+ *
+ * @apiError (Erreur : 400) error Etat incorrect
+ *
+ * @apiErrorExample {json} exemple de réponse en cas d'erreur
+ *     HTTP/1.1 400 Bad Request
+ *
+ *     {
+ *       "error" : "on ne peut pas rétrograder l'état d'une commande: http://localhost/lbsprive/api/commandes/10"
+ *     }
+ */
 $app->post('/commandes/{id}',
 	function (Request $req, Response $resp, $args)
 	{
